1fd61567892e771b2cadd382a6840e03a800f610
James Newell <james@jameslnewell.dev>
2020-04-28 15:19:43 +1000

    check for actually named prop on theme first

--- packages/style/src/get.ts
+++ packages/style/src/get.ts
@@ -1,5 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const get = <V, O = any>(path: string, object: O): V | undefined => {
+export const get = <V, O extends {[segment: string]: any} = {}>(
+  path: string,
+  object: O,
+): V | undefined => {
+  if (Object.prototype.hasOwnProperty.call(object, path)) {
+    return object[path];
+  }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let result: any = object;
   const segments = path.split('.');
