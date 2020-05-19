628c99dace5d35e744bb2bd2c7737b9a9b88bd9b
James Newell <james@jameslnewell.dev>
2020-05-20 03:40:33 +1000

    improving tests

--- packages/style/src/createVariant.tests.tsx
+++ packages/style/src/createVariant.tests.tsx
@@ -1,21 +1,31 @@
 import 'jest-styled-components';
 import React from 'react';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import {render} from '@testing-library/react';
 import {createVariant} from './createVariant';
 
 describe('createVariant()', () => {
   const variant = createVariant({
-    1: {
-      color: 'red',
-    },
+    1: css`
+      color: red;
+    `,
     2: {
       color: 'green',
     },
   });
 
-  test('style for a variant with a styled object is applied', () => {
-    const Component = styled.div({}, variant(2));
+  test('correct style is applied when variant is 1', () => {
+    const Component = styled.div`
+      ${variant(1)}
+    `;
+    const {container} = render(<Component />);
+    expect(container.firstChild).toHaveStyleRule('color', 'red');
+  });
+
+  test('correct style is applied when variant is 2', () => {
+    const Component = styled.div`
+      ${variant(2)}
+    `;
     const {container} = render(<Component />);
     expect(container.firstChild).toHaveStyleRule('color', 'green');
   });
