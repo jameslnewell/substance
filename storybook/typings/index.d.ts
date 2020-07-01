4efea2edff5f00f70246a5cf11accfbdc5741ab8
James Newell <james@jameslnewell.dev>
2020-07-02 00:26:36 +1000

    format

--- storybook/typings/index.d.ts
+++ storybook/typings/index.d.ts
@@ -1,20 +1,20 @@
-import "styled-components";
+import 'styled-components';
 // import {DefaultMedia} from '@substance/style';
 import {DefaultSpace} from '@substance/mixin';
 
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme {
     color: {
       green: {
         light: string;
         dark: string;
-      },
-    },
+      };
+    };
     media: {
-      [media in "sm" | "md" | "lg"]: string; 
-    },
+      [media in 'sm' | 'md' | 'lg']: string;
+    };
     space: {
       [space in DefaultSpace]: string;
-    },
+    };
   }
 }
